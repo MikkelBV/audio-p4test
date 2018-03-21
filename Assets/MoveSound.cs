36d9df27d9392ddfde707464cd8d9b8f6331cc4c
Mikkel Vittrup <mikkel.b.vittrup@gmail.com>
2018-03-21 16:27:37 +0100

    fixed user&camera misalignment bug

--- Assets/MoveSound.cs
+++ Assets/MoveSound.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class MoveSound : MonoBehaviour {
 
     public Transform[] target;
+    private Rigidbody rb;
     public float speed;
-
     private int current;
 	
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
   	void Update () {
-        if (transform.position != target[current].position)
-        {
-            Vector3 pos = Vector3.MoveTowards(transform.position,target[current].position, speed * Time.deltaTime);
-            GetComponent<Rigidbody>().MovePosition(pos);
+        if (transform.position != target[current].position) {
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            rb.MovePosition(pos);
         }
-        else current = (current + 1) % target.Length;
+        else {
+            current = (current + 1) % target.Length;
+        }
 	}
 }
