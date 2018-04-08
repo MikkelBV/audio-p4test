2dc7bcb3645052b6735e8ec54a13c58f889a3403
Mikkel Vittrup <mikkel.b.vittrup@gmail.com>
2018-04-08 21:05:05 +0200

    added sound proximity trigger script

---
+++ Assets/Scripts/SoundSource.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSource : MonoBehaviour {
+	public AudioSource sound;
+	public bool playOnlyOnce = false;
+	private bool shouldPlay = true;
+	
+	void Start ()
+	{
+		sound = GetComponent<AudioSource>();
+		sound.playOnAwake = false;
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (shouldPlay)
+		{
+			sound.Play();
+			if (playOnlyOnce) shouldPlay = false;
+		}
+	}
+}
